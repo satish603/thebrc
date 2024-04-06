62e7509e46bd6bc2f7d77412fb8611285bd04181
Shivanand Burli <shivanand.burli@hpe.com>
2024-04-06 12:50:58 +0530

    added new jar and response data enabled

--- backend/src/main/java/com/thebrchub/rest/controllers/EmailController.java
+++ backend/src/main/java/com/thebrchub/rest/controllers/EmailController.java
@@ -22,7 +22,7 @@ public class EmailController {
 	private ResponseEntity<?> sendEmail(@RequestBody EmailEntity emailEntity) {
 		try {
 			emailService.sendEmail(emailEntity);
-			return new ResponseEntity<>(HttpStatus.OK);
+			return new ResponseEntity<>(emailEntity, HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
