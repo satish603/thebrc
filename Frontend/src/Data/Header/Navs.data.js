1a6b08d808cc8e073a4f02b85d08bcbe782be677
Srikanth <srikanthraj.brchubllp@gmail.com>
2025-11-13 22:12:05 +0530

    updated header

--- Frontend/src/Data/Header/Navs.data.js
+++ Frontend/src/Data/Header/Navs.data.js
@@ -1,7 +1,7 @@
 export default [
     { name: "Home", Id: "home" },
     { name: "About", Id: "about" },
-    // { name: "Portfolio", Id: "portfolio" },
+    { name: "Renderverse", Id: "portfolio" },
     { name: "Client", Id: "client" },
     // { name: "Blog", Id: "blog" },
     { name: "Contact", Id: "contact" },
